34cba2c121e7a270598cbadbe516cacf658984b8
bartlomiejsawicki1 <barteksaw20@wp.pl>
2022-11-17 19:17:18 +0100

    Implement adding restaurants

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,14 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
+}
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+  var restaurantname = '';
+  var burgername = '';
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('dwa'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę restauracji',
+            ),
+            onChanged: (newValule) {
+              setState(() {
+                restaurantname = newValule;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę burgera',
+            ),
+            onChanged: (newValule) {
+              setState(() {
+                burgername = newValule;
+              });
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('restaurants').add({
+                'name': restaurantname,
+                'burger': burgername,
+                'rating': 3.0
+              });
+            },
+            child: const Text(
+              'Dodaj ',
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
