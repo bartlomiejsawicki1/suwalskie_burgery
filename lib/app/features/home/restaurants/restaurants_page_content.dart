e83383ad8bcf6f7ff98e2471011e91928c9f9d1b
bartlomiejsawicki1 <barteksaw20@wp.pl>
2022-11-22 11:57:48 +0100

    Move home and login into features login

--- lib/app/features/home/restaurants/restaurants_page_content.dart
+++ lib/app/features/home/restaurants/restaurants_page_content.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:suwalskie_burgery/app/home/restaurants/cubit/restaurants_cubit.dart';
+import 'package:suwalskie_burgery/app/features/home/restaurants/cubit/restaurants_cubit.dart';
 
 class RestaurantsPageContent extends StatelessWidget {
   const RestaurantsPageContent({
